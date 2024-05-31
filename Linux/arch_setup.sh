a5da03f478e8477b89a777f5bca8a1f921bb9a8b
Paolo <pj.angeloni@gmail.com>
2024-05-31 16:28:40 -0400

    setup linux dotfiles

---
+++ Linux/arch_setup.sh
@@ -0,0 +1,39 @@
+# need to work on i3, polybar, picom, rofi and nerdfonts
+
+#!/bin/bash
+
+myrepo="$HOME/myrepo"
+gitthings="$HOME/Downloads/gitthings"
+scripts="$HOME/Downloads/gitthings/scripts"
+
+#Installing Dependencies (arch only)
+echo "Installing Dependencies"
+sudo pacman -S qtile rofi kdenlive obs-studio sxhkd xclip clipmenu krusader kio-extras discord kitty ranger dunst mpd ncmpcpp mpv 
+neofetch qutebrowser pulsemixer zsh git neovim flatpak picom && paru -S nerd-fonts-complete feh ueberzug
+
+#Download Dots
+echo "Downloading Dotfiles"
+cd && git clone 
+
+mv my-dots myrepo
+
+cd $myrepo
+# ln -s $HOME/myrepo/qtile ~/.config
+ln -s $HOME/myrepo/alacritty ~/.config
+ln -s $HOME/myrepo/zsh ~/.config
+# ln -s $HOME/myrepo/dunst ~/.config
+ln -s $HOME/myrepo/mpd ~/.config
+# ln -s $HOME/myrepo/ncmpcpp ~/.config
+# ln -s $HOME/myrepo/mpv ~/.config
+#rm -r ~/.config/neofetch && ln -s $HOME/myrepo/neofetch ~/.config
+ln -s $HOME/myrepo/picom ~/.config
+# ln -s $HOME/myrepo/ranger ~/.config
+# ln -s $HOME/myrepo/qutebrowser ~/.config
+ln -s $HOME/myrepo/rofi ~/.config
+
+cd $gitthings && git clone 
+cd $scripts
+sudo cp *.sh weather.py /usr/local/bin
+
+cd $HOME/.config
+git clone 
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[Linux](./Linux/) \
 [neovim](./nvim/) \
 [PowerShell](./WindowsTerminal/PowerShell/) \
 [Windows Terminal](./WindowsTerminal/)
@@ -5,3 +6,4 @@
 
 To-Do:
 - include `settings.json` (Windows Terminal) as part of the setup process in PowerShell script
+- customize Linux setup for arch and debian (Parrot OS)
